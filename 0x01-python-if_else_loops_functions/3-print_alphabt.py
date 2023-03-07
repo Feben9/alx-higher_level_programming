bb37b5b4911af6c40a0170f0f07778d068e3ab54
Feben9 <feben7351@gmail.com>
2023-03-07 13:03:34 -0800

    adding the first 4 task solutions

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for ch in range(97, 123):
+	if ch in [101,113]:continue
+	print(chr(ch), end="")
---
+++ 0x01-python-if_else_loops_functions/README.md
@@ -0,0 +1,1 @@
+This is a repo for python project taks for if/else, loops and functions.
