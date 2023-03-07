638f2552e04ac458ff6a0945a8b6bcca15fc4a99
Feben9 <feben7351@gmail.com>
2023-03-07 13:19:27 -0800

    fixing pycodestyle errors

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for ch in range(97, 123):
-	if ch in [101,113]:continue
-	print(chr(ch), end="")
+    if ch in [101, 113]:
+        continue
+    print("{:c}".format(ch), end="")
