bacb23d5b7f3983dd0dbfda26bd95dfcedab5d9f
Feben9 <feben7351@gmail.com>
2023-04-07 14:54:38 -0700

    $msg

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This module is composed by a function that adds two numbers
+"""
+
+def add_integer(a, b=98):
+    """ Function that adds two integer and/or float numbers
+    Args:
+        a: first number
+        b: second number
+    Returns:
+        The addition of the two given numbers
+    Raises:
+        TypeError: If a or b aren't integer and/or float numbers
+    """
+    
+    if (type(a) is not int) and (type(a) is not float):
+        raise TypeError("a must be an integer")
+    if type(a) is float:
+        a = int(a)        
+    if (type(b) is not int) and (type(b) is not float):
+        raise TypeError("b must be an integer")
+    if type(b) is float:
+        b = int(b)
+    return(a + b)
---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+This module is composed by a function that divides the numbers of a matrix
+"""
+def matrix_divided(matrix, div):
+    """ Function that divides the integer/float numbers of a matrix
+    Args:
+        matrix: list of a lists of integers/floats
+        div: number which divides the matrix
+    Returns:
+        A new matrix with the result of the division
+    Raises:
+        TypeError: If the elements of the matrix aren't lists
+                   If the elements of the lists aren't integers/floats
+                   If div is not an integer/float number
+                   If the lists of the matrix don't have the same size
+        ZeroDivisionError: If div is zero
+    """
+
+    if not all(isinstance(matrix, list) for k in matrix):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    
+    for row in matrix:
+        if len(row) ==  len(matrix[0]):
+            pass
+        else:
+            raise TypeError("Each row of the matrix must have the same size")
+    
+    if (type(div) is not int) and (type(div) is not float):
+        raise TypeError("div must be a number")
+    
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            matrix[i][j] = round( (matrix[i][j] / div), 2)
+    
+    return(matrix)
